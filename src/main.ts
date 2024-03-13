c39430e2a1e524f00fa9f0e648d0a3cb939c3a56
HernanElichiry <hernanelichiry148@gmail.com>
2024-03-13 12:05:32 -0300

    elemento estatico

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
+//import { ServeStaticModule } from '@nestjs/serve-static';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+//import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
